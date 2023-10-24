40fc74249cec9b02908854addd5e3f5f64ff6c51
Leandro Apolinário Leite <leandro.ap.leite@gmail.com>
2023-10-24 12:11:51 -0300

    Alterei o script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-const mysql = require('mysql2');
-
-// Configuração da conexão com o MySQL
-const connection = mysql.createConnection({
-    host: 'localhost', // Endereço do servidor MySQL
-    user: 'seu_usuario', // Nome de usuário do MySQL
-    password: 'sua_senha', // Senha do MySQL
-    database: 'escola', // Nome do banco de dados (o mesmo que você criou)
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('Erro de conexão ao banco de dados:', err);
-        return;
-    }
-    console.log('Conexão ao banco de dados MySQL estabelecida.');
-});
-
 const form = document.getElementById("student-form");
 const idInput = document.getElementById("id");
 const nameInput = document.getElementById("name");
@@ -48,36 +30,31 @@ form.addEventListener("submit", function (e) {
         room,
     };
 
-    const sql = "INSERT INTO alunos (id, nome, idade, nota_semestre1, nota_semestre2, nome_professor, numero_sala) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    const values = [id, name, age, semester1, semester2, professor, room];
+    // Enviar os dados do aluno para a API
+    fetch("http://localhost:3000/alunos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(student),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Aluno inserido com sucesso na API:", data);
 
-    connection.query(sql, values, (err, results) => {
-        if (err) {
-            console.error('Erro ao inserir aluno no banco de dados:', err);
-            return;
-        }
-        console.log('Aluno inserido com sucesso no banco de dados.');
+            const listItem = document.createElement("li");
+            listItem.textContent = `ID: ${student.id}, Nome: ${student.name}, Idade: ${student.age}, Nota 1º Semestre: ${student.semester1}, Nota 2º Semestre: ${student.semester2}, Professor: ${student.professor}, Sala: ${student.room}`;
+            studentList.appendChild(listItem);
 
-        const listItem = document.createElement("li");
-        listItem.textContent = `ID: ${student.id}, Nome: ${student.name}, Idade: ${student.age}, Nota 1º Semestre: ${student.semester1}, Nota 2º Semestre: ${student.semester2}, Professor: ${student.professor}, Sala: ${student.room}`;
-        studentList.appendChild(listItem);
-
-        idInput.value = "";
-        nameInput.value = "";
-        ageInput.value = "";
-        semester1Input.value = "";
-        semester2Input.value = "";
-        professorInput.value = "";
-        roomInput.value = "";
-    });
-});
-
-process.on('exit', () => {
-    connection.end((err) => {
-        if (err) {
-            console.error('Erro ao fechar a conexão com o banco de dados:', err);
-        } else {
-            console.log('Conexão ao banco de dados MySQL encerrada.');
-        }
-    });
+            idInput.value = "";
+            nameInput.value = "";
+            ageInput.value = "";
+            semester1Input.value = "";
+            semester2Input.value = "";
+            professorInput.value = "";
+            roomInput.value = "";
+        })
+        .catch((error) => {
+            console.error("Erro ao inserir aluno na API:", error);
+        });
 });
